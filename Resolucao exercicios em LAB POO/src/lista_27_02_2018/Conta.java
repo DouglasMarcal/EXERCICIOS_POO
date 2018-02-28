c5ea229fe45b1dbdd907c9c8fff594b8bba5c5a8
aluno <aluno@lab308.udicentro.local>
2018-02-27 21:31:44 -0300

    teste

--- Resolucao exercicios em LAB POO/src/lista_27_02_2018/Conta.java
+++ Resolucao exercicios em LAB POO/src/lista_27_02_2018/Conta.java
@@ -4,13 +4,44 @@ public class Conta {
 	    String titular;
 	    //..Se declara o atributo como privado ele não
 	    //..sera acessado no metodo main
-	    public int numero = 10;
+	    public int numero;
 	    public double saldo;
 
 	    // ..
+	    public Conta() {
+            // construtor sem argumentos
+        }
+	    public Conta(String titular) {
+            this.titular = "Douglas"; //..
+        }
 
-	    void saca(double valor) {
+	    public String getTitular() {
+			return titular;
+		}
+
+		public void setTitular(String titular) {
+			this.titular = titular;
+		}
+
+		public int getNumero() {
+			return numero;
+		}
+
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+
+		public double getSaldo() {
+			return saldo;
+		}
+
+		public void setSaldo(double saldo) {
+			this.saldo = saldo;
+		}
+
+		void saca(double valor) {
 	        this.saldo = this.saldo - valor; 
 	    }
+	    
 	}
 	
--- Resolucao exercicios em LAB POO/src/lista_27_02_2018/TestaCodigo.java
+++ Resolucao exercicios em LAB POO/src/lista_27_02_2018/TestaCodigo.java
@@ -2,10 +2,20 @@ package lista_27_02_2018;
 
 public class TestaCodigo {
 	public static void main(String[] args) {
-        Conta minhaConta = new Conta();
-        
+        Conta minhaConta = new Conta("");//.. Passo uma string vazia para o construtor
+        //..se tentar ler o saldo como private apresentará
+        //.. mensagem de erro
         minhaConta.saldo = 1000.0;
-        minhaConta.saca(50000); // saldo é só 1000!!	
+        minhaConta.numero = 159;
+
+        //...............................................
+        
+        minhaConta.setSaldo(2000);
+        
+        System.out.println("Nome: " + minhaConta.getTitular() +
+      		  			 "\nNumero: " + minhaConta.getNumero() + 
+      		  			 "\nSaldo: " + minhaConta.getSaldo()) ;
+        
 	}
 
 }
